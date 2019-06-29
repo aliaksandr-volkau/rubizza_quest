f0c4499ba5d21fce4d48b6c5339623a037ab6dbd
fixel92 <goodzoon92@gmail.com>
2019-06-29 09:45:29 +0400

    update cv

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'json'
 
 data = YAML.load_file('cv.yml')
 
+
 uri = URI('https://rubizza.com/register')
 
 http = Net::HTTP.new(uri.host, uri.port)
@@ -11,5 +12,4 @@ http.use_ssl = true
 
 request = Net::HTTP::Post.new(uri, {'Content-Type' => 'application/json'})
 request.body = data.to_json
-
 response = http.request(request)
